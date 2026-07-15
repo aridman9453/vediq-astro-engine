5f1e7eca3413a123721bf55ac8a787c03d15b733
aridman9453 <singharidmanvijaypratap@gmail.com>
2026-07-15 21:30:20 +0530

    Implement Nakshatra list and retrieval function
    Added a list of Nakshatras and a function to retrieve the Nakshatra based on longitude.

---
+++ nakshatra.py
@@ -0,0 +1,42 @@
+NAKSHATRAS = [
+    "Ashwini",
+    "Bharani",
+    "Krittika",
+    "Rohini",
+    "Mrigashira",
+    "Ardra",
+    "Punarvasu",
+    "Pushya",
+    "Ashlesha",
+    "Magha",
+    "Purva Phalguni",
+    "Uttara Phalguni",
+    "Hasta",
+    "Chitra",
+    "Swati",
+    "Vishakha",
+    "Anuradha",
+    "Jyeshtha",
+    "Mula",
+    "Purva Ashadha",
+    "Uttara Ashadha",
+    "Shravana",
+    "Dhanishta",
+    "Shatabhisha",
+    "Purva Bhadrapada",
+    "Uttara Bhadrapada",
+    "Revati"
+]
+
+
+def get_nakshatra(longitude):
+    nak_length = 360 / 27
+
+    index = int(longitude / nak_length)
+
+    pada = int((longitude % nak_length) / (nak_length / 4)) + 1
+
+    return {
+        "nakshatra": NAKSHATRAS[index],
+        "pada": pada
+    }
