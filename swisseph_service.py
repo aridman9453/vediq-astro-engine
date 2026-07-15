2667c113e410923bb6c8f517a04b156d870ccf76
aridman9453 <singharidmanvijaypratap@gmail.com>
2026-07-15 14:29:08 +0530

    Implement birth chart generation function
    Added a function to generate a birth chart based on input parameters.

--- swisseph_service.py
+++ swisseph_service.py
@@ -1,8 +1,8 @@
 import swisseph as swe
 import os
+from datetime import datetime
 
 EPHE_PATH = os.path.join(os.path.dirname(__file__), "ephe")
-
 swe.set_ephe_path(EPHE_PATH)
 
 
@@ -10,4 +10,47 @@ def check_swisseph():
     return {
         "status": "Swiss Ephemeris Loaded",
         "path": EPHE_PATH
+    }
+
+
+def generate_birth_chart(
+    birth_date,
+    birth_time,
+    latitude,
+    longitude,
+    timezone
+):
+    dt = datetime.strptime(
+        f"{birth_date} {birth_time}",
+        "%Y-%m-%d %H:%M"
+    )
+
+    hour = dt.hour + (dt.minute / 60.0)
+
+    utc_hour = hour - timezone
+
+    jd = swe.julday(
+        dt.year,
+        dt.month,
+        dt.day,
+        utc_hour
+    )
+
+    houses = swe.houses(
+        jd,
+        latitude,
+        longitude,
+        b'P'
+    )
+
+    ascendant = houses[1][0]
+
+    sun = swe.calc_ut(jd, swe.SUN)[0][0]
+    moon = swe.calc_ut(jd, swe.MOON)[0][0]
+
+    return {
+        "julian_day": jd,
+        "ascendant": round(ascendant, 6),
+        "sun_longitude": round(sun, 6),
+        "moon_longitude": round(moon, 6)
     }
