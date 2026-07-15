aaf78d7b7e3dbc11fc37cb9601fd965d77921059
aridman9453 <singharidmanvijaypratap@gmail.com>
2026-07-15 17:02:33 +0530

    Integrate Swiss Ephemeris for birth chart generation

--- swisseph_service.py
+++ swisseph_service.py
@@ -46,8 +46,67 @@ def generate_birth_chart(
 
     ascendant = houses[1][0]
 
+    import swisseph as swe
+import os
+from datetime import datetime
+from planetary_positions import get_sign
+
+EPHE_PATH = os.path.join(os.path.dirname(__file__), "ephe")
+swe.set_ephe_path(EPHE_PATH)
+
+
+def check_swisseph():
+    return {
+        "status": "Swiss Ephemeris Loaded",
+        "path": EPHE_PATH
+    }
+
+
+def generate_birth_chart(
+    birth_date,
+    birth_time,
+    latitude,
+    longitude,
+    timezone
+):
+    dt = datetime.strptime(
+        f"{birth_date} {birth_time}",
+        "%Y-%m-%d %H:%M"
+    )
+
+    hour = dt.hour + (dt.minute / 60.0)
+
+    utc_hour = hour - timezone
+
+    jd = swe.julday(
+        dt.year,
+        dt.month,
+        dt.day,
+        utc_hour
+    )
+
+    houses = swe.houses(
+        jd,
+        latitude,
+        longitude,
+        b'P'
+    )
+
+    ascendant = houses[1][0]
+
     sun = swe.calc_ut(jd, swe.SUN)[0][0]
     moon = swe.calc_ut(jd, swe.MOON)[0][0]
+    return {
+    "success": True,
+    "julian_day": round(jd, 6),
+
+    "ascendant": get_sign(ascendant),
+
+    "planets": {
+        "Sun": get_sign(sun),
+        "Moon": get_sign(moon)
+    }
+}
     return {
     "success": True,
     "julian_day": round(jd, 6),
