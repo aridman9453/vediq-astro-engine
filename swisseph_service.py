36624ed271203b969b3c7dc245058678c7d5d470
aridman9453 <singharidmanvijaypratap@gmail.com>
2026-07-15 23:52:44 +0530

    Restore swisseph_service.py

--- swisseph_service.py
+++ swisseph_service.py
@@ -1,10 +1,3 @@
-from house_engine import get_houses
-import swisseph as swe
-import os
-from datetime import datetime
-from planetary_positions import get_sign
-
-EPHE_PATH = os.path.join(os.path.dirname(__file__), "ephe")
 swe.set_ephe_path(EPHE_PATH)
 
 
@@ -59,52 +52,7 @@ swe.set_ephe_path(EPHE_PATH)
 def check_swisseph():
     return {
         "status": "Swiss Ephemeris Loaded",
-        "path": EPHE_PATH
-    }
-
-
-def generate_birth_chart(
-    birth_date,
-    birth_time,
-    latitude,
-    longitude,
-    timezone
-):
-    dt = datetime.strptime(
-        f"{birth_date} {birth_time}",
-        "%Y-%m-%d %H:%M"
-    )
-
-    hour = dt.hour + (dt.minute / 60.0)
-
-    utc_hour = hour - timezone
-
-    jd = swe.julday(
-        dt.year,
-        dt.month,
-        dt.day,
-        utc_hour
-    )
-
-    houses = swe.houses(
-        jd,
-        latitude,
-        longitude,
-        b'P'
-    )
-
-    ascendant = houses[1][0]
-
-    sun = swe.calc_ut(jd, swe.SUN)[0][0]
-    moon = swe.calc_ut(jd, swe.MOON)[0][0]
-    return {
-    "success": True,
-    "julian_day": round(jd, 6),
-
-    "ascendant": get_sign(ascendant),
-
-    "planets": {
-        "Sun": get_sign(sun),
+@@ -59,3 +107,14 @@ def generate_birth_chart(
         "Moon": get_sign(moon)
     }
 }
@@ -114,17 +62,8 @@ def generate_birth_chart(
 
     "ascendant": get_sign(ascendant),
 
-    "houses": get_houses(houses),
-
-        "planets": {
+    "planets": {
         "Sun": get_sign(sun),
-        "Moon": get_sign(moon),
-        "Mercury": get_sign(mercury),
-        "Venus": get_sign(venus),
-        "Mars": get_sign(mars),
-        "Jupiter": get_sign(jupiter),
-        "Saturn": get_sign(saturn),
-        "Rahu": get_sign(rahu),
-        "Ketu": get_sign(ketu)
+        "Moon": get_sign(moon)
     }
 }
