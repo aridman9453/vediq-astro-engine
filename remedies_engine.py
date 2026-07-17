703506e5db2edba9676fc9f8b2a242020dbbac5e
aridman9453 <singharidmanvijaypratap@gmail.com>
2026-07-17 19:18:53 +0530

    Create remedies_engine.py

---
+++ remedies_engine.py
@@ -0,0 +1,8 @@
+def generate_remedies(chart):
+
+    return {
+        "gemstone": "Yellow Sapphire",
+        "mantra": "Om Brim Brihaspataye Namah",
+        "donation": "Donate yellow clothes on Thursday",
+        "fasting": "Thursday fasting recommended"
+    }
