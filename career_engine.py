e10938c6d9e1f4b47c325d04630870860031470d
aridman9453 <singharidmanvijaypratap@gmail.com>
2026-07-17 19:12:10 +0530

    Create career_engine.py

---
+++ career_engine.py
@@ -0,0 +1,10 @@
+def analyze_career(planets):
+
+    return {
+        "career_score": 80,
+        "best_fields": [
+            "Technology",
+            "Business",
+            "Management"
+        ]
+    }
