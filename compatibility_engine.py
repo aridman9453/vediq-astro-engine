d10ded290d0a72e3cad2baab7fd8c83764dc2b42
aridman9453 <singharidmanvijaypratap@gmail.com>
2026-07-17 19:19:48 +0530

    Create compatibility_engine.py

---
+++ compatibility_engine.py
@@ -0,0 +1,7 @@
+def compatibility_report(chart1, chart2):
+
+    return {
+        "score": 82,
+        "status": "Compatible",
+        "summary": "Good emotional and practical compatibility."
+    }
