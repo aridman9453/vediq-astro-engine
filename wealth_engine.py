7efcbbfd11357be17bfd6a532487d0a6268dbede
aridman9453 <singharidmanvijaypratap@gmail.com>
2026-08-04 22:05:40 +0530

    Update wealth_engine.py

--- wealth_engine.py
+++ wealth_engine.py
@@ -1,4 +1,10 @@
-def analyze_wealth(planets):
+from typing import Dict
+
+def analyze_wealth(chart: Dict):
+
+    planets = chart["planets"]
+    houses = chart["houses"]
+    house_lords = chart["house_lords"]
 
     return {
         "wealth_score": 75,
