db8353ec79fd698fca0055ad3c47797cb98952b1
aridman9453 <singharidmanvijaypratap@gmail.com>
2026-08-04 22:09:58 +0530

    Refactor analyze_education to accept a Dict type

--- education_engine.py
+++ education_engine.py
@@ -1,4 +1,11 @@
-def analyze_education(planets):
+from typing import Dict
+
+
+def analyze_education(chart: Dict):
+
+    planets = chart["planets"]
+    houses = chart["houses"]
+    house_lords = chart["house_lords"]
 
     return {
         "education_score": 88,
