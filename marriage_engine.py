dab96f5f06c37f6b3042ccf7ed236c5595538e15
aridman9453 <singharidmanvijaypratap@gmail.com>
2026-08-04 22:07:15 +0530

    Update marriage_engine.py

--- marriage_engine.py
+++ marriage_engine.py
@@ -1,4 +1,11 @@
-def analyze_marriage(planets):
+from typing import Dict
+
+
+def analyze_marriage(chart: Dict):
+
+    planets = chart["planets"]
+    houses = chart["houses"]
+    house_lords = chart["house_lords"]
 
     return {
         "marriage_score": 82,
