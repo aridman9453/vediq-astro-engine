f1384dcaafedad6639a36d7f614d8db1e88f6ea8
aridman9453 <singharidmanvijaypratap@gmail.com>
2026-08-04 22:13:43 +0530

    Update swisseph_service.py

--- swisseph_service.py
+++ swisseph_service.py
@@ -349,8 +349,11 @@ def generate_birth_chart(
     "house_lords": get_house_lords(get_houses(houses))
 }),
 
-"prediction": generate_prediction(planet_data),
-
+"prediction": generate_prediction({
+    "planets": planet_data,
+    "houses": get_houses(houses),
+    "house_lords": get_house_lords(get_houses(houses))
+}),
 "remedies": generate_remedies(planet_data)
 
 }
