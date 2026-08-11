14f8a9f96f0c73cd4a5e176df6cf4988e7c8f981
aridman9453 <singharidmanvijaypratap@gmail.com>
2026-08-11 16:36:23 +0530

    Update ai_astrologer_router.py

--- ai_astrologer_router.py
+++ ai_astrologer_router.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 
 from ai_astrologer_engine import ask_ai_astrologer
+from birth_chart import generate_birth_chart
 
 router = APIRouter(
     prefix="/ai-astrologer",
@@ -18,8 +19,16 @@ class AIQuestion(BaseModel):
 @router.post("/ask")
 def ask(data: AIQuestion):
 
+    birth_chart = generate_birth_chart(
+        birth_date=data.birth_chart["dob"],
+        birth_time=data.birth_chart["tob"],
+        latitude=27.13,
+        longitude=81.95,
+        timezone=5.5
+    )
+
     answer = ask_ai_astrologer(
-        birth_chart=data.birth_chart,
+        birth_chart=birth_chart,
         user_question=data.question,
         palm_report=data.palm_report
     )
